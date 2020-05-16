e708a797fc679453c0d90fbebe295b9bcc38b848
Greg Golovin <golovingreg@gmail.com>
2020-05-16 13:22:19 +0300

    Updates

---
+++ precache-manifest.ed182db0feefc1e33bb2bbfeb8f7cac1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d15a4585cd7659fcebaf26309cd74862",
+    "url": "/facerecognitionapp/index.html"
+  },
+  {
+    "revision": "b25c7465f863e8ae55e1",
+    "url": "/facerecognitionapp/static/css/2.036b41f9.chunk.css"
+  },
+  {
+    "revision": "2f4d5873f047945ffe2b",
+    "url": "/facerecognitionapp/static/css/main.dfbd2f64.chunk.css"
+  },
+  {
+    "revision": "b25c7465f863e8ae55e1",
+    "url": "/facerecognitionapp/static/js/2.2b151ce2.chunk.js"
+  },
+  {
+    "revision": "c97fb91e7dd1c7b19ae67fb152a2e6b2",
+    "url": "/facerecognitionapp/static/js/2.2b151ce2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2f4d5873f047945ffe2b",
+    "url": "/facerecognitionapp/static/js/main.42b6ab01.chunk.js"
+  },
+  {
+    "revision": "0f7d5da407cdb30503f0",
+    "url": "/facerecognitionapp/static/js/runtime-main.cedcfcf4.js"
+  }
+]);
